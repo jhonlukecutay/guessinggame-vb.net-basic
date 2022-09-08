84dcf458bf94b0dd74f21fb43bebbcb1fdd62fa1
Jhon Luke Cutay <113082100+jhonlukecutay@users.noreply.github.com>
2022-09-08 13:07:13 +0800

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Please Enter a number below:")
+        Dim rAnswer As Integer = 3
+        Dim attempt As Integer = 0
+        Dim UInput As Integer = Console.ReadLine
+
+        Do
+            attempt += 1
+
+            If UInput = rAnswer Then
+                Console.WriteLine("You have guessed the correct number.")
+                Console.WriteLine("Correct Number: {0}", rAnswer)
+                Exit Do
+            ElseIf attempt = 5 Then
+                Console.WriteLine("You have reached the maximum number of attempts.")
+                Console.WriteLine("Attempt count: {0}", attempt)
+                Exit Do
+            Else
+                Console.WriteLine("It is not the correct number, please try again.")
+                Console.WriteLine("Attempt count: {0}", attempt)
+                UInput = Console.ReadLine()
+            End If
+        Loop
+
+        Console.ReadLine()
+    End Sub
+
+End Module
